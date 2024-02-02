094b000e2b76add233c3f587f0af9b6a6e9442a6
Michelle Qin <55001348+m-qin@users.noreply.github.com>
2024-02-02 15:25:07 -0500

    initial commit

---
+++ R/hiper_glm.R
@@ -0,0 +1,23 @@
+#' Constructor for objects of the class hiperglm
+#'
+#' @details This function makes a hiperglm object from a design matrix and an outcome vector.
+#'
+#' @param design a n x p design matrix of numeric or factor predictors, possible including an intercept.
+#' @param outcome a n-length numeric vector of outcomes.
+#'
+#' @return a fitted hiperglm object.
+#'
+#' @export
+#'
+hiper_glm <- function(design, outcome){
+  # to do: check that design and outcome are correctly formatted
+
+  # equivalent: hiperglm <- list(); class(hiperglm) <- "hiperglm"
+  hiperglm <- structure(list(design = design,
+                             outcome = outcome,
+                             results = "Yet to be implemented"),
+                        class = "hiperglm")
+  return(hiperglm)
+}
+
+
---
+++ R/utils.R
@@ -0,0 +1,41 @@
+#' Printed summary of hiperglm object
+#'
+#' @details This function returns prints summary characteristics of a fitted hiperglm object.
+#'
+#' @param x a fitted hiperglm object.
+#' @param ... further arguments passed to or from other methods.
+#'
+#' @export
+#'
+
+print.hiperglm <- function(x, ...){
+  warning("Yet to be implemented")
+}
+
+
+#' Estimated coefficients of hiperglm object
+#'
+#' @details This function returns the estimated regression coefficients of a fitted hiperglm object.
+#'
+#' @param hiperglm a fitted hiperglm object.
+#'
+#' @export
+#'
+
+coef.hiperglm <- function(hiperglm){
+  warning("Yet to be implemented")
+}
+
+
+#' Estimated variance-covariance matrix of hiperglm object
+#'
+#' @details This function returns the estimated variance-covariance matrix of a fitted hiperglm object.
+#'
+#' @param hiperglm a fitted hiperglm object.
+#'
+#' @export
+#'
+
+vcov.hiperglm <- function(hiperglm){
+  warning("Yet to be implemented")
+}
