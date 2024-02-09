19810793334cd0aa95903d040c55d8c35e9f2b5e
Michelle Qin <55001348+m-qin@users.noreply.github.com>
2024-02-09 16:51:33 -0500

    add another test
    first test passes, second test fails. not sure whether bfgs or pseudoinv is the problem

--- tests/testthat/test-bfgs_mle.R
+++ tests/testthat/test-bfgs_mle.R
@@ -1,8 +1,22 @@
 test_that("BFGS and pseudo-inverse LS estimates of linear model (n=100, p=2) are close", {
+  ### test #1
+
   # get simulated data
-  n_obs <- 100
-  n_pred <- 2
-  data <- simulate_data(n_obs = n_obs, n_pred = n_pred, model = "linear", intercept = 1,
+  data <- simulate_data(n_obs = 100, n_pred = 2, model = "linear", intercept = 1,
+                        coef_true = NULL, design = NULL, seed = 140778, signal_to_noise = 0.1)
+
+  # get MLE estimated by pseudoinverse and MLE estimated by BFGS
+  mle_pseudoinv <- pseudoinverse_mle(data$design, data$outcome)
+  mle_bfgs <- bfgs_mle(data$design, data$outcome)
+
+  # compare
+  expect_equal(are_all_close(mle_bfgs, mle_pseudoinv), TRUE)
+
+
+  ### test #2
+
+  # get simulated data
+  data <- simulate_data(n_obs = 500, n_pred = 5, model = "linear", intercept = 1,
                         coef_true = NULL, design = NULL, seed = 140778, signal_to_noise = 0.1)
 
   # get MLE estimated by pseudoinverse and MLE estimated by BFGS
