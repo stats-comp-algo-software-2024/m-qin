03f6380b0b73fde6163ba9b6e0cef64fac953075
Michelle Qin <55001348+m-qin@users.noreply.github.com>
2024-02-09 16:30:02 -0500

    add more tests for gradient
    test-bfgs_mle is still failing, not sure if the issue is in the gradient calculation

--- tests/testthat/test-gradient_and_loglik.R
+++ tests/testthat/test-gradient_and_loglik.R
@@ -1,7 +1,43 @@
 test_that("gradient calculation matches numerical gradient calculation", {
+  ### test #1
+
   # get simulated data
   n_obs <- 80
   n_pred <- 3
+  data <- simulate_data(n_obs = n_obs, n_pred = n_pred, model = "linear", intercept = 1,
+                        coef_true = NULL, design = NULL, seed = 140778, signal_to_noise = 0.1)
+
+  # compute gradient at specific value of beta
+  beta0 <- rep(0, n_pred + 1)
+  analytic_gradient <- gradient_of_loglik(data$design, data$outcome, beta0)
+  numeric_gradient <- approx_grad_of_func(function(beta) loglik(data$design, data$outcome, beta), beta0)
+
+  # compare
+  expect_equal(are_all_close(analytic_gradient, numeric_gradient), TRUE)
+
+
+  ### test #2
+
+  # get simulated data
+  n_obs <- 200
+  n_pred <- 6
+  data <- simulate_data(n_obs = n_obs, n_pred = n_pred, model = "linear", intercept = 1,
+                        coef_true = NULL, design = NULL, seed = 140778, signal_to_noise = 0.1)
+
+  # compute gradient at specific value of beta
+  beta0 <- rep(0, n_pred + 1)
+  analytic_gradient <- gradient_of_loglik(data$design, data$outcome, beta0)
+  numeric_gradient <- approx_grad_of_func(function(beta) loglik(data$design, data$outcome, beta), beta0)
+
+  # compare
+  expect_equal(are_all_close(analytic_gradient, numeric_gradient), TRUE)
+
+
+  ### test #3
+
+  # get simulated data
+  n_obs <- 100
+  n_pred <- 2
   data <- simulate_data(n_obs = n_obs, n_pred = n_pred, model = "linear", intercept = 1,
                         coef_true = NULL, design = NULL, seed = 140778, signal_to_noise = 0.1)
 
