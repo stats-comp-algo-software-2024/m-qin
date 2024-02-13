f03648b5d2cb1c998c295d18a33b5ce880429574
Michelle Qin <55001348+m-qin@users.noreply.github.com>
2024-02-13 16:45:58 -0500

    remove unnecessary comments (pt 1)
    don't worry about transpose thing; solve() doesn't have a special parameter for that, unlike chol()

--- R/pseudoinverse_mle.R
+++ R/pseudoinverse_mle.R
@@ -12,7 +12,7 @@ pseudoinverse_mle <- function(design, outcome){
   # we want beta_hat = (X^T X)^{-1} X^T y
   # i.e., X^T X beta_hat = X^T y
   # so beta_hat is the solution to solve(X^T X, X^T y)
-  beta_hat <- solve(t(design) %*% design, t(design) %*% outcome) # to do: think about whether t(design) is computationally intensive; should I save it somewhere?
+  beta_hat <- solve(t(design) %*% design, t(design) %*% outcome)
 
   # return as vector, not p x 1 matrix, for conformity with other functions
   beta_hat <- as.vector(beta_hat)
