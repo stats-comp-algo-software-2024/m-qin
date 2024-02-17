ac06369fcf9acab992d0950b66608f6f83380c88
Michelle Qin <55001348+m-qin@users.noreply.github.com>
2024-02-16 23:42:22 -0500

    set up logistic regression test

--- tests/testthat/test-bfgs_mle.R
+++ tests/testthat/test-bfgs_mle.R
@@ -17,3 +17,16 @@ test_that("BFGS and pseudo-inverse LS estimates of linear model (n=500, p=5) are
 
   expect_true(are_all_close(mle_bfgs, mle_pseudoinv, abs_tol = 1e-6, rel_tol = 1e-4))
 })
+
+test_that("Newton and BFGS outputs coincide on logit model (n=32, p=4)", {
+  data <- simulate_data(n_obs = 32, n_pred = 4, model = 'logit', seed = 1918)
+  design <- data$design
+  outcome <- data$outcome
+
+  via_newton_out <- hiper_glm(design, outcome, model = 'logit')
+  via_bfgs_out <- hiper_glm(design, outcome, model = 'logit', option = list(mle_solver = 'BFGS'))
+
+  expect_true(are_all_close(coef(via_newton_out),
+                            coef(via_bfgs_out),
+                            abs_tol = 1e-2, rel_tol = 1e-2))
+})
